6ce9cda2c04eed5e522dd10a5fda06e057d81c0a
Guilherme Thomazi Bonicontro <thomazi@linux.com>
2024-09-01 13:28:34 +0200

    fix: adding command description to output

--- cmd/root.go
+++ cmd/root.go
@@ -17,12 +17,9 @@ import (
 var rootCmd = &cobra.Command{
 	Use:   "capable",
 	Short: "Interact with Capacities.io API",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Long: `Capable is CLI tool is designed to interact with the Capacities.io API, 
+enabling users to send daily entries and weblinks to the Capacities platform 
+directly from the command line.`,
 }
 
 // Execute runs the root command and exits the application if an error occurs
